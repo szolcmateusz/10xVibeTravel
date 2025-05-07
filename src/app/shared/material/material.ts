cfa44b958cb754b7f7eb59f78c50c7a06f8b8fab
Mateusz Szolc <mateusz.szolc@ceneo.pl>
2025-05-07 19:55:19 +0200

    feat(material): add MatToolbarModule to MATERIAL_MODULES for enhanced layout options

--- src/app/shared/material/material.ts
+++ src/app/shared/material/material.ts
@@ -8,6 +8,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const MATERIAL_MODULES = [
   MatButtonModule,
@@ -18,7 +19,8 @@ const MATERIAL_MODULES = [
   MatFormFieldModule,
   MatInputModule,
   MatSelectModule,
-  MatDialogModule
+  MatDialogModule,
+  MatToolbarModule
 ];
 
 @NgModule({
