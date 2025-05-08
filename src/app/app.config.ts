8cd395406d38887be3e891b469f1bedc72773904
Mateusz Szolc <mateusz.szolc@ceneo.pl>
2025-05-08 09:13:47 +0200

    feat(trip-plans): enhance app configuration by adding animations, date locale, and native date adapter providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,19 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
+import { provideHttpClient } from '@angular/common/http';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' },
+    MatNativeDateModule,
+    provideHttpClient(),
+    provideNativeDateAdapter(),  // Adding Native Date Adapter provider
+  ]
 };
